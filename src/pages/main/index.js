52d62110c8e6924ced849aaab3c9603dd998760d
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-10-21 23:11:34 -0300

    config mapbox

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,38 +1,76 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { StatusBar, View } from "react-native";
+import PropTypes from 'prop-types';
 import MapboxGL from "@react-native-mapbox-gl/maps";
 
 MapboxGL.setAccessToken("pk.eyJ1IjoiYW5kcmUtZWR1YXJkbyIsImEiOiJjazFydWJnNDMwOTQzM2NueXJzOHVobXJzIn0.3zXesN6jxrEtueZsKbnQYg");
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  container: {
-    height: 300,
-    width: 300,
-    backgroundColor: "tomato"
-  },
-  map: {
-    flex: 1
-  }
-});
+import api from '../../services/api';
 
-export default class App extends Component {
-  componentDidMount() {
-    MapboxGL.setTelemetryEnabled(false);
+import { Container, AnnotationContainer, AnnotationText } from './styles';
+
+export default class Main extends Component {
+  static navigationOptions = {
+    header: null,
   }
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      state: PropTypes.shape({
+        params: PropTypes.shape({
+          token: PropTypes.string,
+        }),
+      }),
+    }).isRequired,
+  }
+
+  state = {
+    locations: [],
+  }
+
+  async componentDidMount() {
+    try {
+      const response = await api.get('/properties', {
+        params: {
+          latitude: -27.210768,
+          longitude: -49.644018,
+        },
+      });
+
+      this.setState({ locations: response.data });
+
+      console.tron.log(response);
+    } catch (err) {
+      console.tron.log(err);
+    }
+  }
+
+  renderLocations = () => (
+    this.state.locations.map(location => (
+      <MapboxGL.PointAnnotation
+        id={location.id.toString()}
+        coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
+      >
+        <AnnotationContainer>
+          <AnnotationText>{location.price}</AnnotationText>
+        </AnnotationContainer>
+        <MapboxGL.Callout title={location.title}/>
+      </MapboxGL.PointAnnotation>
+    ))
+  )
 
   render() {
     return (
-      <View style={styles.page}>
-        <View style={styles.container}>
-          <MapboxGL.MapView style={styles.map} />
-        </View>
-      </View>
+      <Container>
+        <StatusBar barStyle="light-content" />
+        <MapboxGL.MapView
+          centerCoordinate={[-49.6446024, -27.2108001]}
+          style={{ flex: 1 }}
+          styleURL={MapboxGL.StyleURL.Dark}
+        >
+          { this.renderLocations() }
+        </MapboxGL.MapView>
+      </Container>
     );
   }
 }
