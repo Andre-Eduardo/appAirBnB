56f2b58b95486de494c662be2769073824c98121
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-10-22 10:46:33 -0300

    tela de cadrastro de imagens

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StatusBar, View } from "react-native";
+import { StatusBar, View , Dimensions,Modal} from "react-native";
 import PropTypes from 'prop-types';
 import MapboxGL from "@react-native-mapbox-gl/maps";
 
@@ -7,7 +7,27 @@ MapboxGL.setAccessToken("pk.eyJ1IjoiYW5kcmUtZWR1YXJkbyIsImEiOiJjazFydWJnNDMwOTQz
 import { RNCamera } from 'react-native-camera';
 import api from '../../services/api';
 
-import { Container, AnnotationContainer, AnnotationText } from './styles';
+import {
+  Container,
+  AnnotationContainer,
+  AnnotationText,
+  NewButtonContainer,
+  ButtonsWrapper,
+  CancelButtonContainer,
+  SelectButtonContainer,
+  ButtonText,
+  Marker,
+  ModalContainer,
+  ModalImagesListContainer,
+  ModalImagesList,
+  ModalImageItem,
+  ModalButtons,
+  CameraButtonContainer,
+  CancelButtonText,
+  ContinueButtonText,
+  TakePictureButtonContainer,
+  TakePictureButtonLabel,
+} from './styles';
 
 export default class Main extends Component {
   static navigationOptions = {
@@ -26,39 +46,164 @@ export default class Main extends Component {
 
   state = {
     locations: [],
-  }
+    newRealty: false,
+    cameraModalOpened: false,
+    dataModalOpened: false,
+    realtyData: {
+      location: {
+        latitude: null,
+        longitude: null,
+      },
+      name: '',
+      price: '',
+      address: '',
+      images: [],
+    },
+};
+async componentDidMount() {
+  try {
+    const response = await api.get('/properties', {
+      params: {
+        latitude: -27.210768,
+        longitude: -49.644018,
+      },
+    });
 
-  async componentDidMount() {
-    try {
-      const response = await api.get('/properties', {
-        params: {
-          latitude: -27.210768,
-          longitude: -49.644018,
+    this.setState({ locations: response.data });
+  } catch (err) {
+    console.tron.log(err);
+  }
+}
+
+handleNewRealtyPress = () => this.setState({ newRealty: !this.state.newRealty })
+
+handleCameraModalClose = () => this.setState({ cameraModalOpened: !this.state.cameraModalOpened })
+
+handleDataModalClose = () => this.setState({
+  dataModalOpened: !this.state.dataModalOpened,
+  cameraModalOpened: false,
+})
+
+handleGetPositionPress = async () => {
+  try {
+    const [longitude, latitude] = await this.map.getCenter();
+    this.setState({
+      cameraModalOpened: true,
+      realtyData: {
+        ...this.state.realtyData,
+        location: {
+          latitude,
+          longitude,
         },
-      });
-
-      this.setState({ locations: response.data });
-
-      console.tron.log(response);
-    } catch (err) {
-      console.tron.log(err);
-    }
+      },
+    });
+  } catch (err) {
+    console.tron.log(err);
   }
+}
 
-  renderLocations = () => (
-    this.state.locations.map(location => (
-      <MapboxGL.PointAnnotation
-        id={location.id.toString()}
-        coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
-      >
-        <AnnotationContainer>
-          <AnnotationText>{location.price}</AnnotationText>
-        </AnnotationContainer>
-        <MapboxGL.Callout title={location.title}/>
-      </MapboxGL.PointAnnotation>
-    ))
+handleTakePicture = async () => {
+  if (this.camera) {
+    const options = { quality: 0.5, base64: true, forceUpOrientation: true, fixOrientation: true, };
+    const data = await this.camera.takePictureAsync(options)
+    const { realtyData } = this.state;
+    this.setState({ realtyData: {
+      ...realtyData,
+      images: [
+        ...realtyData.images,
+        data,
+      ]
+    }})
+  }
+}
+
+renderConditionalsButtons = () => (
+  !this.state.newRealty ? (
+    <NewButtonContainer onPress={this.handleNewRealtyPress}>
+      <ButtonText>Novo Imóvel</ButtonText>
+    </NewButtonContainer>
+  ) : (
+    <ButtonsWrapper>
+      <SelectButtonContainer onPress={this.handleGetPositionPress}>
+        <ButtonText>Selecionar localização</ButtonText>
+      </SelectButtonContainer>
+      <CancelButtonContainer onPress={this.handleNewRealtyPress}>
+        <ButtonText>Cancelar</ButtonText>
+      </CancelButtonContainer>
+    </ButtonsWrapper>
   )
+)
 
+renderLocations = () => (
+  this.state.locations.map(location => (
+    <MapboxGL.PointAnnotation
+      id={location.id.toString()}
+      coordinate={[parseFloat(location.longitude), parseFloat(location.latitude)]}
+    >
+      <AnnotationContainer>
+        <AnnotationText>{location.price}</AnnotationText>
+      </AnnotationContainer>
+      <MapboxGL.Callout title={location.title} />
+    </MapboxGL.PointAnnotation>
+  ))
+)
+
+renderMarker = () => (
+  this.state.newRealty && !this.state.cameraModalOpened ? (
+    <Marker resizeMode="contain" source={require('../../images/marker.png')} />
+  ) : null
+)
+
+renderImagesList = () => (
+  this.state.realtyData.images.length !== 0 ? (
+    <ModalImagesListContainer>
+      <ModalImagesList horizontal>
+        { this.state.realtyData.images.map(image => (
+          <ModalImageItem source={{ uri: image.uri }} resizeMode="stretch" />
+        ))}
+      </ModalImagesList>
+    </ModalImagesListContainer>
+  ) : null
+)
+
+renderCameraModal = () => (
+  <Modal
+    visible={this.state.cameraModalOpened}
+    transparent={false}
+    animationType="slide"
+    onRequestClose={this.handleCameraModalClose}
+  >
+    <ModalContainer>
+      <ModalContainer>
+        <RNCamera
+          ref={camera => {
+            this.camera = camera;
+          }}
+          style={{ flex: 1 }}
+          type={RNCamera.Constants.Type.back}
+          autoFocus={RNCamera.Constants.AutoFocus.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          permissionDialogTitle={"Permission to use camera"}
+          permissionDialogMessage={
+            "We need your permission to use your camera phone"
+          }
+        />
+        <TakePictureButtonContainer onPress={this.handleTakePicture}>
+          <TakePictureButtonLabel />
+        </TakePictureButtonContainer>
+      </ModalContainer>
+      { this.renderImagesList() }
+      <ModalButtons>
+        <CameraButtonContainer onPress={this.handleCameraModalClose}>
+          <CancelButtonText>Cancelar</CancelButtonText>
+        </CameraButtonContainer>
+        <CameraButtonContainer onPress={this.handleDataModalClose}>
+          <ContinueButtonText>Continuar</ContinueButtonText>
+        </CameraButtonContainer>
+      </ModalButtons>
+    </ModalContainer>
+  </Modal>
+)
   render() {
     return (
       <Container>
@@ -67,9 +212,16 @@ export default class Main extends Component {
           centerCoordinate={[-49.6446024, -27.2108001]}
           style={{ flex: 1 }}
           styleURL={MapboxGL.StyleURL.Dark}
+          ref={map => {
+            this.map = map;
+          }}
         >
+
           { this.renderLocations() }
         </MapboxGL.MapView>
+        { this.renderConditionalsButtons() }
+        { this.renderMarker() }
+        { this.renderCameraModal() }
       </Container>
     );
   }
